ae82fef5f9e6c8c9f3bfdffd8b2abc0abcfd303c
Laureen Chepkoech <lawrynmarusoi@gmail.com>
2026-01-27 02:05:31 +0300

    Add Streamlit app script

---
+++ Loan_prediction.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1713d811-7378-444a-86a2-8fac8b204f6d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
